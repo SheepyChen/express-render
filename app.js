4721751aa11540fd0c586f5411c99cef3cda6b61
Flora Chen <flora8411@gmail.com>
2023-04-11 21:54:42 +0800

    test

--- app.js
+++ app.js
@@ -1,29 +1,27 @@
-const express = require("express");
+// pathRewrite: {
+//   "^/111": "/api/v1/rest/datastore/301000000A-000082-053",
+//   "^/110": "/api/v1/rest/datastore/301000000A-000082-049",
+//   "^/109": "/api/v1/rest/datastore/301000000A-000082-045",
+//   "^/108": "/api/v1/rest/datastore/301000000A-000082-041",
+//   "^/107": "/api/v1/rest/datastore/301000000A-000082-033",
+// },const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
+const port = process.env.PORT || 10000;
 
-// Proxy requests to the appropriate API based on the endpoint
 app.use(
-  "/",
+  "/api/v1/rest/datastore/301000000A-000082-041",
   createProxyMiddleware({
-    target: "https://od.moi.gov.tw/api/v1/rest/datastore/301000000A-000082-053",
+    target: "https://od.moi.gov.tw",
     changeOrigin: true,
     pathRewrite: {
-      "^/111": "/api/v1/rest/datastore/301000000A-000082-053",
-      "^/110": "/api/v1/rest/datastore/301000000A-000082-049",
-      "^/109": "/api/v1/rest/datastore/301000000A-000082-045",
-      "^/108": "/api/v1/rest/datastore/301000000A-000082-041",
-      "^/107": "/api/v1/rest/datastore/301000000A-000082-033",
+      "^/api/v1/rest/datastore/301000000A-000082-041":
+        "/api/v1/rest/datastore/301000000A-000082-041",
     },
   })
 );
 
-// Serve static files from the 'build' directory
-app.use(express.static("build"));
-
-// Start the server
-const PORT = process.env.PORT || 3100;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
